ca869b54ef709bc87d451a8f1fcc4cf5b01e9cc5
MaryLynJuana <mary_juana@ukr.net>
2021-05-01 16:54:52 +0300

    test(api): Cover balancing algorithm with unit tests

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,35 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
+
+var (
+	baseAddress = "172.19.0."
+	expectedServers = []string{
+		"server1:8080",
+		"server2:8080",
+		"server3:8080",
+		"server1:8080",
+	}
+)
 
 func TestBalancer(t *testing.T) {
 	// TODO: Реалізуйте юніт-тест для балансувальникка.
+	for i := 0; i <= 3; i++ {
+		addr := fmt.Sprintf("%s%d", baseAddress, i + 1)
+		for j := 0; j <= 3; j++ {
+			server, err := balanceRequest(addr)
+			if err != nil {
+				t.Fatal(err)
+			}
+			expected := expectedServers[i]
+			if server != expected {
+				t.Errorf(
+					"Balancing algorithm returned wrong server: expected %s, got %s",
+					expected, server)
+			}
+		}
+	}
 }
