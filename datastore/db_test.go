530af72f97070bee478b38e7d24b3e15a07f89b1
AntonPodash <apodash01@gmail.com>
2021-05-29 23:53:04 +0300

    task2

--- datastore/db_test.go
+++ datastore/db_test.go
@@ -7,6 +7,34 @@ import (
 	"testing"
 )
 
+var (
+	pairs = [][]string{
+		{"key1", "value1"},
+		{"key2", "value2"},
+		{"key3", "value3"},
+	}
+
+	newPairs = [][]string{
+		{"key2", "value3"},
+		{"key3", "value4"},
+	}
+
+	morePairs = [][]string{
+		{"key1", "value1"},
+		{"key2", "value2"},
+		{"key3", "value3"},
+		{"key4", "value4"},
+		{"key5", "value5"},
+		{"key6", "value6"},
+		{"key7", "value7"},
+		{"key8", "value8"},
+		{"key9", "value9"},
+		{"key10", "value10"},
+		{"key11", "value11"},
+		{"key12", "value12"},
+	}
+)
+
 func TestDb_Put(t *testing.T) {
 	dir, err := ioutil.TempDir("", "test-db")
 	if err != nil {
@@ -18,15 +46,8 @@ func TestDb_Put(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer db.Close()
 
-	pairs := [][]string {
-		{"key1", "value1"},
-		{"key2", "value2"},
-		{"key3", "value3"},
-	}
-
-	outFile, err := os.Open(filepath.Join(dir, outFileName))
+	outFile, err := os.Open(filepath.Join(dir, segmentPrefix+activeSuffix))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -52,6 +73,10 @@ func TestDb_Put(t *testing.T) {
 		t.Fatal(err)
 	}
 	size1 := outInfo.Size()
+	/*
+		the current database has 10 MB active block size, so merge func won't be called
+		and this "file growth" test will be deterministic
+	*/
 
 	t.Run("file growth", func(t *testing.T) {
 		for _, pair := range pairs {
@@ -64,7 +89,7 @@ func TestDb_Put(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		if size1 * 2 != outInfo.Size() {
+		if size1*2 != outInfo.Size() {
 			t.Errorf("Unexpected size (%d vs %d)", size1, outInfo.Size())
 		}
 	})
@@ -81,12 +106,39 @@ func TestDb_Put(t *testing.T) {
 		for _, pair := range pairs {
 			value, err := db.Get(pair[0])
 			if err != nil {
-				t.Errorf("Cannot put %s: %s", pairs[0], err)
+				t.Errorf("Cannot get %s: %s", pairs[0], err)
 			}
 			if value != pair[1] {
 				t.Errorf("Bad value returned expected %s, got %s", pair[1], value)
 			}
 		}
 	})
+}
+func TestDb_Segmentation(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test-db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
 
+	db, err := NewDbSized(dir, 50)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, pair := range pairs {
+		err = db.Put(pair[0], pair[1])
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	files, err := ioutil.ReadDir(dir)
+	if len(files) != 2 {
+		t.Errorf("Unexpected segment count (%d vs %d)", len(files), 2)
+	}
+
+	if err := db.Close(); err != nil {
+		t.Fatal(err)
+	}
 }
